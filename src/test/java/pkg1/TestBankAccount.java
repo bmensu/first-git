e6b4a706906c59572f7f54fdd537b8dd82ba940a
bmensu <soumen_bhakta@yahoo.com>
2015-07-05 19:22:12 -0400

    6

--- src/test/java/pkg1/TestBankAccount.java
+++ src/test/java/pkg1/TestBankAccount.java
@@ -6,4 +6,11 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
+  @Test
+  public void testDebitWithSufficientFunds() {
+    BankAccount account = new BankAccount (100);
+    double amount = account.debit(60);
+    Assert.assertEquals(40.0, amount);
+  }
+
 }
